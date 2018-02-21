cd670251b509dc117c80687181221efc5c6320bb
Zharphyn <brad@zharphyn.ca>
2018-02-21 01:46:24 +0000

    Finished URL Shortening pt1

--- express_server.js
+++ express_server.js
@@ -1,14 +1,29 @@
 /*jshint esversion: 6 */
 
-var express = require("express");
-var app = express(); // starts express
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express(); // starts express
+const PORT = process.env.PORT || 8080; // default port 8080
 app.set('view engine', 'ejs'); // sets the view engine
 
-var urlDatabase = {
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// Genereate a random 6 char string of Upper/Lower case letters and numbers
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
 
 app.get("/", (request, response) => {
   response.end("Hello!");
@@ -24,6 +39,15 @@ app.get("/urls.json", (request, response) => {
 
 app.get("/urls", (request, response) => {
   response.render("urls_index", { urlDatabase: urlDatabase });
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/hello", (request, response) => {
